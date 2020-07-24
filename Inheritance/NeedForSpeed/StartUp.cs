8e39ed1ec7d3e21b4f00b7653eab72832861cb66
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-24 18:22:37 +0300

    New task

---
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(300,100);
+            Car car = new Car(300,100);
+
+            vehicle.Drive(10);
+            car.Drive(10);
+
+            System.Console.WriteLine(vehicle.Fuel);
+            System.Console.WriteLine(car.Fuel);
+        }
+    }
+}
---
+++ Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public void Drive(double kilometers)
+        {
+            if (kilometers * this.FuelConsumption <= this.Fuel)
+            {
+                this.Fuel -= kilometers * this.FuelConsumption;
+            }
+            
+        }
+    }
+}
