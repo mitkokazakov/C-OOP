8fde937a17b654c238860823e465b3acde5a3714
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-30 23:05:32 +0300

    Another task has been uploaded

---
+++ Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,92 @@
+﻿
+
+using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private const int MIN_VALUE_SIDE = 0;
+        private const string ERROR_MESAGE = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length 
+        {
+            get 
+            {
+                return this.length;
+            }
+
+            private set
+            {
+                if (value <= MIN_VALUE_SIDE)
+                {
+                    throw new ArgumentException(String.Format(ERROR_MESAGE,nameof(this.Length)));
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                if (value <= MIN_VALUE_SIDE)
+                {
+                    throw new ArgumentException(String.Format(ERROR_MESAGE, nameof(this.Width)));
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                if (value <= MIN_VALUE_SIDE)
+                {
+                    throw new ArgumentException(String.Format(ERROR_MESAGE, nameof(this.Height)));
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height); 
+        }
+
+        public double LateralSurafceArea()
+        {
+            return  (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+        }
+
+        public double Volume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+    }
+}
---
+++ Encapsulation/ClassBoxData/Engine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    class Engine
+    {
+        public Engine()
+        {
+
+        }
+
+        public void Run()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurafceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
