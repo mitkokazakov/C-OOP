4e42ab7806e709a2d3afae8f124fb5eb7874e381
mitkokazakov <DimitarKazakov@students.softuni.com>
2020-07-31 19:15:13 +0300

    New task

---
+++ PizzaCalories/Common/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories.Common
+{
+    public static class GlobalConstants
+    {
+        public static double CaloriesPerGram = 2.0;
+    }
+}
