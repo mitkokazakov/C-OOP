47011bcc66d1cfbb3264e343d032c8f416889f3c
mitkokazakov <DimitarKazakov@students.softuni.bg>
2020-12-07 18:10:12 +0200

    Class Product has been added

---
+++ Exam - 16 August 2020/OnlineShop/Models/Products/Product.cs
@@ -0,0 +1,110 @@
+﻿using OnlineShop.Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products
+{
+    public abstract class Product : IProduct
+    {
+        private int id;
+        private string manufacturer;
+        private string model;
+        private decimal price;
+        private double overallPerformance;
+
+        public Product()
+        {
+
+        }
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidProductId);
+                }
+
+                this.id = value;
+            }
+        }
+        public string Manufacturer 
+        {
+            get
+            {
+                return this.manufacturer;
+            }
+
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidManufacturer);
+                }
+
+                this.manufacturer = value;
+            }
+        }
+        public string Model 
+        {
+            get
+            {
+                return this.model;
+            }
+
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidModel);
+                }
+
+                this.model = value;
+            }
+        }
+        public decimal Price
+        {
+
+            get
+            {
+                return this.price;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+
+                this.price = value;
+            }
+        }
+
+        public double OverallPerformance
+        {
+            get
+            {
+                return this.overallPerformance;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOverallPerformance);
+                }
+
+                this.overallPerformance = value;
+            }
+        }
+    }
+
+}
+
